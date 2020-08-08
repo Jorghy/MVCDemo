392975d98b4eebc7e48fdca0cdc51beb2450f6fb
Jorghy <36571093+Jorghy@users.noreply.github.com>
2020-08-08 22:14:50 +0200

    Add migration and scaffolding files.

---
+++ MVCDemo/Data/MvcMovieContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCDemo.Models;
+
+namespace MVCDemo.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- MVCDemo/Startup.cs
+++ MVCDemo/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCDemo.Data;
 
 namespace MVCDemo
 {
@@ -24,6 +21,7 @@ namespace MVCDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
